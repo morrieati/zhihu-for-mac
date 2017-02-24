b18c86a06429e7870efa8494307ad554babf60ee
Morris Lew <morrislew@outlook.com>
2017-02-24 09:52:45 +0800

    feature: navbar

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const electron = require('electron')
+const {
+    app,
+    BrowserWindow
+} = require('electron')
 const path = require('path')
 const url = require('url')
 
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+// const app = electron.app
+// const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
+
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        titleBarStyle: 'hidden',
+        titleBarStyle: 'hidden-inset',
     })
     // load 'index.html'
     mainWindow.loadURL(url.format({
